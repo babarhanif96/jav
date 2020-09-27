a968caa76c488be7e18a6e98c081b034d62ba53c
unknown <babarhanif96@gmail.com>
2020-09-27 03:02:36 -0700

    first commit

---
+++ Untitled2.cpp
@@ -0,0 +1,42 @@
+#include<iostream> 
+#include<stack> 
+#include<string> 
+using namespace std; 
+bool ArePair(char opening,char closing) 
+{ 
+ if(opening == '(' && closing == ')') return true; 
+ else if(opening == '{' && closing == '}') return true; 
+ else if(opening == '[' && closing == ']') return true; 
+ return false; 
+} 
+bool AreParanthesesBalanced(string exp) 
+{ 
+ stack<char>  S; 
+ for(int i =0;i<exp.length();i++) 
+ { 
+  if(exp[i] == '(' || exp[i] == '{' || exp[i] == '[') 
+   S.push(exp[i]); 
+  else if(exp[i] == ')' || exp[i] == '}' || exp[i] == ']') 
+  { 
+   if(S.empty() || !ArePair(S.top(),exp[i])) 
+    return false; 
+   else 
+    S.pop(); 
+  } 
+ } 
+ return S.empty() ? true:false; 
+} 
+ 
+int main() 
+{ 
+ /*Code to test the function AreParanthesesBalanced*/ 
+ string expression; 
+ cout<<"Enter an expression:  ";  
+ cin>>expression; 
+ if(AreParanthesesBalanced(expression)) 
+  cout<<"Balanced\n"; 
+ else 
+  cout<<"Not Balanced\n"; 
+} 
+
+
